efec0fd1511bc18c4652fae12eec3759e6058b8b
Abdulbasit762 <154410960+Abdulbasit762@users.noreply.github.com>
2024-03-06 03:56:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+ //       Assignment 4
+
+// var variable1, variable2, variable3;
+
+//      Legal variable 
+
+// firstName
+// age
+// user_id
+// myVar
+// _myVariable
+
+
+//                   Illegal variable 
+
+// 123variable
+// ++variable
+// user-id
+// @my_variable
+// %my_variable
+
+
+document.write("<h1>Rules for Naming JS Variables</h1>");
+document.write("<p>Variable names can only contain letters, digits, underscores, and dollar signs. For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name</p>");
+document.write("<p>Variable names are case-sensitive</p>");
+document.write("<p>Variable names should not be JS reserved keywords</p>");
